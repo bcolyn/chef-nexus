d98153f4ea571ad01ff4fba7f5881129d7217a9b
Benny Colyn <benny.colyn@thomascookonline.com>
2014-09-25 17:31:16 +0200

    basic java install with serverspec test

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'nexus'
+maintainer       'Benny Colyn'
+maintainer_email 'Benny.Colyn@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures nexus'
+long_description 'Installs/Configures nexus'
+version          '0.1.0'
+
+depends "java"
