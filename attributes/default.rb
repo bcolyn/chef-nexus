b4fb9417aa76d261360b72ba2363b943c26d2ca7
Benny Colyn <benny.colyn@thomascookonline.com>
2014-10-22 10:28:15 +0200

    idea warnings in attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 default['nexus'] =
     {
-        'download_url' => 'http://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz',
-        'version' => '2.9.2',
-        'sha256' => '2c03fb47bb4c3a48801e9a198b4f7445f6af270be02ee0d1ff9902ef70e255a7',
-        'user' => 'nexus',
-        'install_dir' => '/opt/nexus',
-        'data_dir' => '/data/nexus',
-        'jetty_port' => '8080',
-        'jetty_iface' => '0.0.0.0',
-        'cmd' => 'nexus',
-        'opts' => 'start',
+        download_url: 'http://download.sonatype.com/nexus/oss/nexus-latest-bundle.tar.gz',
+        version: '2.9.2',
+        sha256: '2c03fb47bb4c3a48801e9a198b4f7445f6af270be02ee0d1ff9902ef70e255a7',
+        user: 'nexus',
+        install_dir: '/opt/nexus',
+        data_dir: '/data/nexus',
+        jetty_port: '8080',
+        jetty_iface: '0.0.0.0',
+        cmd: 'nexus',
+        opts: 'start',
     }
